6a264fcce75b2fbe46118d4591b6bdc81c99907c
Ning Han <ning.han@intel.com>
2023-01-17 09:34:24 +0800

    update lspsaga configurations
    Signed-off-by: Ning Han <ning.han@intel.com>

--- lua/ninghan/plugins/lsp/lspsaga.lua
+++ lua/ninghan/plugins/lsp/lspsaga.lua
@@ -1,18 +1,22 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-  return
+	return
 end
 
-saga.init_lsp_saga({
-  -- keybinds for navigation in lspsaga window
-  move_in_saga = { prev = "<C-k>", next = "<C-j>" },
-  -- use enter to open file with finder
-  finder_action_keys = {
-    open = "<CR>",
-  },
-  -- use enter to open file with definition preview
-  definition_action_keys = {
-    edit = "<CR>",
-  },
-})
+saga.setup({})
+
+local keymap = vim.keymap.set
+
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+-- rename
+keymap("n", "gr", "<cmd>Lspsaga rename<CR>")
+-- goto definition
+keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+-- call hierarchy
+keymap("n", "<leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+keymap("n", "<leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+-- toggle outline
+keymap("n", "<leader>co", "<cmd>Lspsaga outline<CR>")
+-- toggle float terminal
+keymap({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
