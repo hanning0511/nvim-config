e3e193c7099b9d84bbd38fd4dd3dccdd61ec0dc7
Han, Ning <ning.han@intel.com>
2023-03-20 09:46:05 +0800

    usage onelight colorschema
    Signed-off-by: Han, Ning <ning.han@intel.com>

--- lua/ninghan/core/colorscheme.lua
+++ lua/ninghan/core/colorscheme.lua
@@ -1,7 +1,5 @@
-local status, _ = pcall(vim.cmd, "colorscheme onedark")
+local status, _ = pcall(vim.cmd, "colorscheme onelight")
 if not status then
 	print("Colorscheme not found!")
 	return
 end
-
-vim.opt.background = "dark"
