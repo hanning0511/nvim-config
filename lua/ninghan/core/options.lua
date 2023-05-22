ec1466bd30054742be571ff8fea3b2cbaafffc1b
Ning Han <ning.han@intel.com>
2023-05-22 15:00:15 +0800

    change tab settings for jsx
    Signed-off-by: Ning Han <ning.han@intel.com>

--- lua/ninghan/core/options.lua
+++ lua/ninghan/core/options.lua
@@ -45,3 +45,7 @@ api.nvim_create_autocmd("FileType", {
 	pattern = "javascript",
 	command = "setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2",
 })
+api.nvim_create_autocmd("FileType", {
+	pattern = "javascriptreact",
+	command = "setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2",
+})
